a9b822aba1d5a3ad9306ef1da3f000f3c0672ea1
Chris Carpentier <coredump@gmail.com>
2015-11-13 16:07:45 -0500

    Rename files

--- src/main/java/com/zoniex/halloweenMod/HalloweenMod.java
+++ src/main/java/com/zoniex/halloweenMod/HalloweenMod.java
@@ -1,16 +1,16 @@
-package com.example.examplemod;
+package com.zoneix.halloweenMod;
 
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = HalloweenMod.MODID, version = HalloweenMod.VERSION)
+public class HalloweenMod
 {
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
-    
+    public static final String MODID = "Halloween Mod";
+    public static final String VERSION = "1.0.0";
+
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
