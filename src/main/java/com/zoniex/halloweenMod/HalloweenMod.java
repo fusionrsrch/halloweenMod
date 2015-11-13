9b7efe96af226375201f5ecf9a3e7b8e94e7d972
Chris Carpentier <coredump@gmail.com>
2015-11-13 16:14:24 -0500

    Adjust Name

--- src/main/java/com/zoniex/halloweenMod/HalloweenMod.java
+++ src/main/java/com/zoniex/halloweenMod/HalloweenMod.java
@@ -5,11 +5,12 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = HalloweenMod.MODID, version = HalloweenMod.VERSION)
+@Mod(modid = HalloweenMod.MODID, version = HalloweenMod.VERSION, name = HalloweenMod.NAME)
 public class HalloweenMod
 {
-    public static final String MODID = "Halloween Mod";
+    public static final String MODID = "HalloweenMod";
     public static final String VERSION = "1.0.0";
+    public static final String NAME = "Halloween Mod";
 
     @EventHandler
     public void init(FMLInitializationEvent event)
